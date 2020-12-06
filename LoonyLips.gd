6708ab40f0b58d72233bcec15f268de36d964563
donsvederus <58320530+donsvederus@users.noreply.github.com>
2020-12-06 01:36:54 -0700

    0.13 Buttons

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -5,10 +5,16 @@ func _ready():
 	var story = "Once upon a time %s watched %s and thought it was the %s movie ever made in %s."
 	$VBoxContainer/DisplayText.text = story % prompts
 	
+	
 func _on_PlayerText_text_entered(new_text):
 	update_DisplayText(new_text)
 	
 	
+func _on_TextureButton_pressed():
+	var words = $VBoxContainer/HBoxContainer/PlayerText.text
+	update_DisplayText(words)
+	
+
 func update_DisplayText(words):
 	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/PlayerText.clear()
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
