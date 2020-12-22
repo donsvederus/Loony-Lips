aa5885d053e4eba0c776a968890b77357e7056bc
donsvederus <58320530+donsvederus@users.noreply.github.com>
2020-12-22 01:37:38 -0700

    0.15 Queue_free and reload_current_scene
    new functions

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -11,14 +11,14 @@ func _ready():
 	DisplayText.text = "Welcome to Loony Lips!  We're going to tell a story and have a wonderful time! "
 	check_player_words_length()
 	
-	
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
 	
-	
 func _on_TextureButton_pressed():
-	add_to_player_words()
-	
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 	
 func add_to_player_words():
 	player_words.append(PlayerText.text)
@@ -26,22 +26,22 @@ func add_to_player_words():
 	PlayerText.clear()
 	check_player_words_length()
 	
-	
 func is_story_done():
 	return player_words.size() == prompts.size()
 	
-
 func check_player_words_length():
 	if is_story_done():
-		tell_story()
+		end_game()
 	else:
 		prompt_player()
-		
 		
 func tell_story():
 	DisplayText.text = story % player_words
 	
-	
 func prompt_player():
 	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
-
+	
+func end_game():
+	PlayerText.queue_free()
+	$VBoxContainer/HBoxContainer/Label.text = "Again!"
+	tell_story()
